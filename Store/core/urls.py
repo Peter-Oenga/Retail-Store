a97dbd0bb4b6ebf45cb0825bcf3099d804335fbc
Peter-Oenga <poenga230@gmail.com>
2024-03-25 23:20:47 +0300

    Login and Logout

--- Store/core/urls.py
+++ Store/core/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("about/", views.about, name='about')
+    path("about/", views.about, name='about'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout')
 ]
--- Store/core/views.py
+++ Store/core/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . models import product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -8,3 +10,28 @@ def index(request):
 
 def about(request):
     return render(request, "about.html", {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'Logged out')
+    return redirect('index')
+
+    
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        
+        
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have succesfully logged in!')
+            return redirect('index')
+        
+        else:
+            messages.success(request, "There was a problem please try again later")
+            return redirect('login')
+
+    else:
+        return render(request, "login.html", {})
