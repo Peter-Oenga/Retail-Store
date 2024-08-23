8421435ddcf166764f907a6e6e8e13b250e4501d
Peter-Oenga <poenga230@gmail.com>
2024-08-23 04:10:31 +0300

    Collection os static files

--- backend/Store/settings.py
+++ backend/Store/settings.py
@@ -159,7 +159,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
--- backend/Store/urls.py
+++ backend/Store/urls.py
@@ -12,3 +12,6 @@ urlpatterns = [
     path('payments/', include('payment.urls')),
     path('', include('shipping.urls'))
   ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
